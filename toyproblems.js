66a487ca85a883371617843e88b752e483fafb96
Ty Frith <tyfrith@gmail.com>
2015-06-06 23:52:59 -0700

    update

--- toyproblems.js
+++ toyproblems.js
@@ -1317,6 +1317,38 @@ function DashInsert(str) {
 
 //-------------------------------------------------
 
+ // have the function ThreeFiveMultiples(num) return the sum of all the multiples of 3 and 5 that are below num. 
+ // For example: if num is 10, the multiples of 3 and 5 that are below 10 are 3, 5, 6, and 9, and adding them up you get 23, so your program should return 23. 
+ // The integer being passed will be between 1 and 100. 
+
+ function ThreeFiveMultiples(num) {
+    
+    var sumArr = [];
+
+  for (var i = 3; i < num; i++) {
+
+        if(i % 3 == 0) {
+            sumArr.push(i);
+        }
+
+        if(i % 5 == 0) {
+            sumArr.push(i)
+        }
+    }
+    
+    var total = 0;
+    $.each(sumArr,function() {
+        total += this;
+    });
+
+return total;
+
+}
+
+//---------------------------------------------------------------------------------
+
+
+
 
 
 
